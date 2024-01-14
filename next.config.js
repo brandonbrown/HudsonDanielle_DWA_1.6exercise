7f922dfc8e7df4af1215284e7009778948c41312
brandonbrown <brandon@centraflux.com>
2024-01-14 14:09:07 -0500

    next config replace

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-// next.config.js
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
 }
 
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  output: 'export',
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-}
+module.exports = nextConfig
